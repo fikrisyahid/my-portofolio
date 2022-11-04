bb8ff08c2c89b736a75898c3ec15d056d64122c9
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-11-04 15:32:46 +0700

    add sx props

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,15 +1,18 @@
+import { SxProps, Theme } from "@mui/material";
 import Box from "@mui/material/Box";
 import { ReactNode } from "react";
 
 interface MainCardProps {
   children: ReactNode;
   backgroundColor?: string;
+  sx?: SxProps<Theme> | undefined
 }
 
-export default function MainCard({ children, backgroundColor }: MainCardProps) {
+export default function MainCard({ children, backgroundColor, sx }: MainCardProps) {
   return (
     <Box
       sx={{
+        ...sx,
         borderRadius: 3,
         backgroundColor: backgroundColor || "#172731",
         padding: 2,
