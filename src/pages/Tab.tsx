9ceb0b6e1b497016482ee4e7e955c862b62543b6
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-11-04 10:01:17 +0700

    change grid to box

--- src/pages/Tab.tsx
+++ src/pages/Tab.tsx
@@ -1,4 +1,4 @@
-import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { useContext } from "react";
 import { PageContext } from "../context/page-context";
@@ -6,67 +6,58 @@ import { PageContext } from "../context/page-context";
 export default function Tab() {
   const { currentPage, setCurrentPage } = useContext(PageContext);
   return (
-    <Grid container justifyContent="center" spacing={2}>
-      <Grid item>
-        <Button
-          variant="text"
-          sx={{
-            fontSize: 16,
-            fontWeight: 700,
-            color: "white",
-            backgroundColor:
-              currentPage === "stack" ? "#25343F" : "transparent",
-          }}
-          onClick={() => setCurrentPage("stack")}
-        >
-          My Stack
-        </Button>
-      </Grid>
-      <Grid item>
-        <Button
-          variant="text"
-          sx={{
-            fontSize: 16,
-            fontWeight: 700,
-            color: "white",
-            backgroundColor:
-              currentPage === "experience" ? "#25343F" : "transparent",
-          }}
-          onClick={() => setCurrentPage("experience")}
-        >
-          Work Experience
-        </Button>
-      </Grid>
-      <Grid item>
-        <Button
-          variant="text"
-          sx={{
-            fontSize: 16,
-            fontWeight: 700,
-            color: "white",
-            backgroundColor:
-              currentPage === "certification" ? "#25343F" : "transparent",
-          }}
-          onClick={() => setCurrentPage("certification")}
-        >
-          Certification
-        </Button>
-      </Grid>
-      <Grid item>
-        <Button
-          variant="text"
-          sx={{
-            fontSize: 16,
-            fontWeight: 700,
-            color: "white",
-            backgroundColor:
-              currentPage === "about-me" ? "#25343F" : "transparent",
-          }}
-          onClick={() => setCurrentPage("about-me")}
-        >
-          About Me
-        </Button>
-      </Grid>
-    </Grid>
+    <Box display="flex" justifyContent="center" gap={3}>
+      <Button
+        variant="text"
+        sx={{
+          fontSize: 16,
+          fontWeight: 700,
+          color: "white",
+          backgroundColor: currentPage === "stack" ? "#25343F" : "transparent",
+        }}
+        onClick={() => setCurrentPage("stack")}
+      >
+        My Stack
+      </Button>
+      <Button
+        variant="text"
+        sx={{
+          fontSize: 16,
+          fontWeight: 700,
+          color: "white",
+          backgroundColor:
+            currentPage === "experience" ? "#25343F" : "transparent",
+        }}
+        onClick={() => setCurrentPage("experience")}
+      >
+        Work Experience
+      </Button>
+      <Button
+        variant="text"
+        sx={{
+          fontSize: 16,
+          fontWeight: 700,
+          color: "white",
+          backgroundColor:
+            currentPage === "certification" ? "#25343F" : "transparent",
+        }}
+        onClick={() => setCurrentPage("certification")}
+      >
+        Certification
+      </Button>
+      <Button
+        variant="text"
+        sx={{
+          fontSize: 16,
+          fontWeight: 700,
+          color: "white",
+          backgroundColor:
+            currentPage === "about-me" ? "#25343F" : "transparent",
+        }}
+        onClick={() => setCurrentPage("about-me")}
+      >
+        About Me
+      </Button>
+    </Box>
   );
 }
