54777161ec5c299a9f1f981395c33405ba81d552
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-11-04 17:00:02 +0700

    add view credential button

--- src/components/CertBox.tsx
+++ src/components/CertBox.tsx
@@ -2,6 +2,7 @@ import Item from "./Item";
 import Dialog from "@mui/material/Dialog";
 import { useState } from "react";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 interface CertBoxProps {
   src: string;
@@ -12,21 +13,39 @@ interface CertBoxProps {
 export default function CertBox({ url, src, alt }: CertBoxProps) {
   const [dialogOpen, setDialogOpen] = useState(false);
   return (
-    <Item hover>
-      <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+    <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
+      <Item hover>
+        <Dialog
+          fullWidth
+          maxWidth="md"
+          open={dialogOpen}
+          onClose={() => setDialogOpen(false)}
+        >
           <img alt={alt} src={src} style={{ width: "100%", height: "100%" }} />
-          {url && (
-            <Button variant="contained" color="primary">
-              View Credential
-            </Button>
-          )}
-      </Dialog>
-      <img
-        alt={alt}
-        src={src}
-        style={{ width: "20vw", height: "15vw", cursor: "pointer" }}
-        onClick={() => setDialogOpen(true)}
-      />
-    </Item>
+        </Dialog>
+        <img
+          alt={alt}
+          src={src}
+          style={{ width: "20vw", height: "15vw", cursor: "pointer" }}
+          onClick={() => setDialogOpen(true)}
+        />
+      </Item>
+      {url && (
+        <a href={url} target="_blank" style={{ textDecoration: "none" }}>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "#3f5465",
+              color: "white",
+              ":hover": {
+                backgroundColor: "#4e708d",
+              },
+            }}
+          >
+            View Credential
+          </Button>
+        </a>
+      )}
+    </Box>
   );
 }
